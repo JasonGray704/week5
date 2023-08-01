d3fd0bc33edb802bb4c8c5dbfeaaf1c193c6e9b5
Bubba Gray <bubbagray@Bubbas-Air.attlocal.net>
2023-08-01 13:16:12 -0400

    week05 coding assignment

---
+++ Application.java
@@ -0,0 +1,14 @@
+package com.promineotech2;
+
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Logger Logger = new SpacedLogger();
+		
+		Logger.log("Jason");
+		
+	}
+
+}
---
+++ Logger.java
@@ -0,0 +1,8 @@
+package com.promineotech2;
+
+public interface Logger {
+
+	public void log(String log);
+	public void error(String error);
+	
+}
